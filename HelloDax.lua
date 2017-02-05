a8cea603911170e413d03247381a72e545a98bd0
DAXweb <daxweb_nolimit@libero.it>
2017-02-05 07:49:24 +0100

    First Commit

---
+++ HelloDax.lua
@@ -0,0 +1,11 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: ifull
+-- Date: 10/22/14
+-- Time: 10:36 AM
+-- To change this template use File | Settings | File Templates.
+--
+
+function HelloDax_OnLoad()
+	print("Bastronzo DAX.  Build info: ", GetBuildInfo());
+end
